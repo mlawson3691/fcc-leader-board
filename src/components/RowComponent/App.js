e8830fb820b581c2df3f74e87010ff68b1fac827
Mark Lawson <mlawson3691@gmail.com>
2017-01-13 18:44:25 -0800

    add RowComponent to fill dynamically

---
+++ src/components/RowComponent/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import Request from 'superagent';
+import './App.css';
+
+export default class RowComponent extends Component {
+  render() {
+    return (
+      <div>
+        <div className='rank'>{this.props.index+1}</div>
+        <div className='name'>
+          <img className='avatar' src={this.props.user.img} alt='Avatar' />
+          {this.props.user.username}
+        </div>
+        <div className='month'>{this.props.user.recent}</div>
+        <div className='total'>{this.props.user.alltime}</div>
+      </div>
+    );
+  }
+}
