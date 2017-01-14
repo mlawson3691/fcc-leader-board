291e3065cb096e53cf9d05ed24d3b716a79979ab
Mark Lawson <mlawson3691@gmail.com>
2017-01-13 18:44:57 -0800

    update BoardComponent with RowComponent

--- src/components/BoardComponent/App.js
+++ src/components/BoardComponent/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Request from 'superagent';
+import RowComponent from './../RowComponent/App.js';
 import './App.css';
 
 export default class BoardComponent extends Component {
@@ -7,8 +8,8 @@ export default class BoardComponent extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      recent: [],
-      alltime: []
+      viewing: 'recent',
+      users: []
     };
   }
 
@@ -20,8 +21,7 @@ export default class BoardComponent extends Component {
     var url = 'https://fcctop100.herokuapp.com/api/fccusers/top/recent';
     var _this = this;
     return Request.get(url).then(function(data) {
-      console.log(data.body[0]);
-      _this.setState({ recent: data.body });
+      _this.setState({ users: data.body });
     });
   }
 
@@ -29,18 +29,48 @@ export default class BoardComponent extends Component {
     var url = 'https://fcctop100.herokuapp.com/api/fccusers/top/alltime';
     var _this = this;
     return Request.get(url).then(function(data) {
-      _this.setState({ alltime: data.body });
+      _this.setState({ users: data.body });
     });
+  }
+
+  _changeDisplay() {
+    if (this.state.viewing === 'recent') {
+      this.setState({viewing: 'alltime'});
+      this.GetAlltime();
+    } else {
+      this.setState({viewing: 'recent'});
+      this.GetRecent();
+    }
   }
 
   render() {
     return (
       <div>
-        {this.state.recent.map( (user) => {
-          return (
-            <div class='name'>{user.username}</div>
-          );
-        }, this)}
+        <div className='switchBtn' onClick={this._changeDisplay.bind(this)}>Switch Ranking</div>
+        <div className='chart'>
+          <div className='header-row'>
+            <div className='rank'>RANK</div>
+            <div className='name'>CAMPER</div>
+            <div className='month'>LAST 30 DAYS</div>
+            <div className='total'>ALL TIME</div>
+          </div>
+          {this.state.users.map( (user, index) => {
+            var rowClass;
+            if (index % 2 === 0) {
+              rowClass = 'row';
+            } else {
+              rowClass = 'row shade';
+            }
+            return (
+              <div className={rowClass}>
+                <RowComponent
+                  user={user}
+                  index={index}
+                />
+              </div>
+            );
+          }, this)}
+        </div>
       </div>
     );
   }
